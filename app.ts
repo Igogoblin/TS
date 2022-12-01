835c4135165b5d9c5623c4117e132ee437f77a68
Ihar Skavysh <igogoblin@gmail.com>
2022-12-01 12:20:31 +0300

    feat: add types and function

--- app.ts
+++ app.ts
@@ -1,9 +1,23 @@
-const t: string = 'it works';
-const tes: number = 123;
+let test1: number = +'123';
+let test2: number = +'456';
 
-function sayName(name:string):void{
-  console.log(name);
+console.log(test1 + test2);
+
+let arr: string[] = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr[0]);
+
+let arr1: number[] = [1,2];
+
+let arr2: Array<string> = ['a','b'];
+let arr3: Array<number> = [1,2];
+
+let obj = {a: 1, b: 2, c: 3};
+let res: number = 0;
+
+for (let key in obj) {
+	let elem: number = obj[key];
+	res += elem;
 }
-sayName('davichto');
-console.log(t);
-type SomeType = string | null | undefined;
+
+console.log(res);
+
