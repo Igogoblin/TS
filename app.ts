8233851a65230824327a6d58cfce72de1427a6dc
Ihar Skavysh <igogoblin@gmail.com>
2022-12-01 21:15:08 +0300

    docs: chenge tsconfig.json add outDir and target

--- app.ts
+++ app.ts
@@ -3,8 +3,8 @@ let test2: number = +'456';
 
 console.log(test1 + test2);
 
-let arr: string[] = ['a', 'b', 'c', 'd', 'e'];
-console.log(arr[0]);
+let arr0: string[] = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr0[0]);
 
 let arr1: number[] = [1,2];
 
@@ -12,12 +12,18 @@ let arr2: Array<string> = ['a','b'];
 let arr3: Array<number> = [1,2];
 
 let obj = {a: 1, b: 2, c: 3};
-let res: number = 0;
+// let res: number = 0;
 
-for (let key in obj) {
-	let elem: number = obj[key];
-	res += elem;
+function sum(arr:Array<number>):number {
+	
+	let res:any;
+	for (let num of arr) {
+		res += num;
+	}
+	
+	return res;
 }
 
-console.log(res);
-
+function func(elem: HTMLElement, text: string):void {
+	elem.textContent = text;
+}
