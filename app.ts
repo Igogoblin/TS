7424d671365af18121f016b6fe28caa1748a7d37
Ihar Skavysh <igogoblin@gmail.com>
2023-02-01 19:25:53 +0300

    init: start course

--- app.ts
+++ app.ts
@@ -1,9 +1,1 @@
-const t: string = 'it works';
-const tes: number = 123;
-
-function sayName(name:string):void{
-  console.log(name);
-}
-sayName('davichto');
-console.log(t);
-type SomeType = string | null | undefined;
+const a = 1;
